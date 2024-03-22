ef0fadadaf1c152242cf66d2f9c371a410b1dfd6
Dean Woods <dmwoods38@gmail.com>
2024-03-22 15:08:00 -0700

    Add copilot to neovim. Add VS Code to setup script.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.yaml" },
         { import = "lazyvim.plugins.extras.lang.docker" },
         { import = "lazyvim.plugins.extras.lang.rust" },
+        { import = "lazyvim.plugins.extras.coding.copilot" },
         -- import/override with your plugins
         { import = "plugins" },
     },
